a24af33ef667dd43c65f5efc28afd5f781969f41
piasmajumdar <piasmajumdar@gmail.com>
2026-02-02 10:17:09 +0530

    task 1 completed

---
+++ task-1.js
@@ -0,0 +1,8 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(num1, num2, num3, num4) {
+    const result = num1 * num2 * num3 * num4;
+    return result;
+}
+
+console.log(multiply(2, 3, 4, 5));
